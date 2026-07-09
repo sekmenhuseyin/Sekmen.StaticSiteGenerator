482038837b64378fca7e59d9042a76dc2ec18b6d
Hüseyin Sekmenoğlu <huseyin@sekmen.dev>
2026-07-09 22:34:25 +0300

    updated usings

--- UmbracoTestProject/GlobalUsings.cs
+++ UmbracoTestProject/GlobalUsings.cs
@@ -1,2 +1,5 @@
-﻿global using Microsoft.AspNetCore.Rewrite;
+﻿global using Microsoft.AspNetCore.Builder;
+global using Microsoft.AspNetCore.Rewrite;
+global using Umbraco.Cms.Core.DependencyInjection;
 global using Umbraco.Community.HtmlExporter.Startup;
+global using Umbraco.Extensions;
